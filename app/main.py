8892140069335c44952565a819c9c1d1c6a54e33
rushman95 <rushab21@gmail.com>
2023-10-06 16:22:54 +1100

    catboost update

--- app/main.py
+++ app/main.py
@@ -1,20 +1,20 @@
 import os
 import pandas as pd
-#import xgboost
 from fastapi import FastAPI, HTTPException
 from datetime import date, datetime
 from starlette.responses import JSONResponse
 from joblib import load
-from prophet.serialize import model_to_json, model_from_json
+from prophet.serialize import model_from_json
 
 app = FastAPI()
 
 lr_pipe = load('../models/lr_pipeline.joblib')
 
+cat = load('../models/CatBoost-nocalendar.joblib')
+
 with open('../models/serialized_model.json', 'r') as fin:
     prophet_pipe = model_from_json(fin.read())  # Save model
 
-print(lr_pipe)
 
 # Solution:
 @app.get("/")
@@ -23,13 +23,14 @@ def read_root():
 
 @app.get('/health', status_code=200)
 def healthcheck():
-    return 'Linear Regressor is all ready to go!'
+    return 'This is a cool assignment! Thank you Anthony :)'
 
 def format_features(
     item_id: str,
     store_id: str,
     d: date
     ):
+    # Generating date and category features
     return {
         'item_id': [item_id],
         'dept_id': [item_id.rsplit("_", 1)[0]],
@@ -56,7 +57,8 @@ def predict(
 
     df = pd.DataFrame(features)
 
-    pred = lr_pipe.predict(df)
+    # Predicting
+    pred = cat.predict(df)
 
     return JSONResponse(pred.tolist())
 
@@ -70,14 +72,12 @@ def format_date(
 @app.get("/sales/national")
 def forecast(date: date):
 
-    #date = format_date(date)
-    print(date)
-    # Generate a date range for the week
+    # Generate a date range for the week and dataframe with these dates
     end_date = date + pd.DateOffset(days=6)
     date_range = pd.date_range(start=date, end=end_date)
-
     df = pd.DataFrame({'ds': date_range})
 
+    # Forecasting on dataframe
     forecast = prophet_pipe.predict(df)
 
     result = forecast[['ds', 'yhat']]
